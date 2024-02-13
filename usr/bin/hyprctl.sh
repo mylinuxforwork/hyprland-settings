a5b7688365f5af8606dbcca66444359082b27d4d
Stephan Raabe <mail@stephan-raabe.de>
2024-02-13 07:09:09 +0100

    Updates

--- usr/bin/hyprctl.sh
+++ usr/bin/hyprctl.sh
@@ -11,11 +11,8 @@ jq -c '.[]' hyprctl.json | while read i; do
     _key() {
         echo $1 | jq -r '.key'
     }
-    # key=$($i | jq -r '.key')
     key=$(_key $i)
     val=$(_val $i)
     echo "Execute: hyprctl keyword $key $val"
     hyprctl keyword $key $val
 done
-
-
