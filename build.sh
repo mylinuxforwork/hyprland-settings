b2f1765d8a783dd9b04090737a9e4cd7c5f9cec8
Stephan Raabe <mail@stephan-raabe.de>
2024-04-10 14:40:42 +0200

    Updates

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd ..
 ARCH=x86_64 appimagetool ml4w-hyprland-settings
+echo ":: AppImage created"
 cp ML4W_Hyprland_Settings-x86_64.AppImage ~/ml4w-hyprland-settings/
-# cp ML4W_Hyprland_Settings-x86_64.AppImage ~/dotfiles-versions/dotfiles/apps/
-cp ML4W_Hyprland_Settings-x86_64.AppImage ~/dotfiles/apps/
+cp ML4W_Hyprland_Settings-x86_64.AppImage ~/dotfiles-versions/dotfiles/apps/
+echo ":: AppImage copied to ~/dotfiles-versions/dotfiles/apps/"
--- usr/bin/library.sh
+++ usr/bin/library.sh
@@ -1,13 +1,174 @@
 #!/bin/bash
+#  _     _ _                           
+# | |   (_) |__  _ __ __ _ _ __ _   _  
+# | |   | | '_ \| '__/ _` | '__| | | | 
+# | |___| | |_) | | | (_| | |  | |_| | 
+# |_____|_|_.__/|_|  \__,_|_|   \__, | 
+#                               |___/  
+#  
+# by Stephan Raabe (2023) 
+# ----------------------------------------------------- 
 
-# _replaceInFile $startMarker $endMarker $customtext $targetFile
+# ------------------------------------------------------
+# Function: Is package installed
+# ------------------------------------------------------
+_isInstalledPacman() {
+    package="$1";
+    check="$(sudo pacman -Qs --color always "${package}" | grep "local" | grep "${package} ")";
+    if [ -n "${check}" ] ; then
+        echo 0; #'0' means 'true' in Bash
+        return; #true
+    fi;
+    echo 1; #'1' means 'false' in Bash
+    return; #false
+}
+
+_isInstalledYay() {
+    package="$1";
+    check="$(yay -Qs --color always "${package}" | grep "local" | grep "\." | grep "${package} ")";
+    if [ -n "${check}" ] ; then
+        echo 0; #'0' means 'true' in Bash
+        return; #true
+    fi;
+    echo 1; #'1' means 'false' in Bash
+    return; #false
+}
+
+_isFolderEmpty() {
+    folder="$1"
+    if [ -d $folder ] ;then
+        if [ -z "$(ls -A $folder)" ]; then
+            echo 0
+        else
+            echo 1
+        fi
+    else
+        echo 1
+    fi
+}
+
+# ------------------------------------------------------
+# Function Install all package if not installed
+# ------------------------------------------------------
+_installPackagesPacman() {
+    toInstall=();
+    for pkg; do
+        if [[ $(_isInstalledPacman "${pkg}") == 0 ]]; then
+            echo ":: ${pkg} is already installed.";
+            continue;
+        fi;
+        toInstall+=("${pkg}");
+    done;
+
+    if [[ "${toInstall[@]}" == "" ]] ; then
+        # echo "All pacman packages are already installed.";
+        return;
+    fi;
+
+    # printf "Package not installed:\n%s\n" "${toInstall[@]}";
+    sudo pacman --noconfirm -S "${toInstall[@]}";
+}
+
+_forcePackagesPacman() {
+    toInstall=();
+    for pkg; do
+        toInstall+=("${pkg}");
+    done;
+
+    if [[ "${toInstall[@]}" == "" ]] ; then
+        # echo "All pacman packages are already installed.";
+        return;
+    fi;
+
+    # printf "Package not installed:\n%s\n" "${toInstall[@]}";
+    sudo pacman --noconfirm -S "${toInstall[@]}" --ask 4;
+}
+
+_installPackagesYay() {
+    toInstall=();
+    for pkg; do
+        if [[ $(_isInstalledYay "${pkg}") == 0 ]]; then
+            echo ":: ${pkg} is already installed.";
+            continue;
+        fi;
+        toInstall+=("${pkg}");
+    done;
+
+    if [[ "${toInstall[@]}" == "" ]] ; then
+        # echo "All packages are already installed.";
+        return;
+    fi;
+
+    # printf "AUR packags not installed:\n%s\n" "${toInstall[@]}";
+    yay --noconfirm -S "${toInstall[@]}";
+}
+
+_forcePackagesYay() {
+    toInstall=();
+    for pkg; do
+        toInstall+=("${pkg}");
+    done;
+
+    if [[ "${toInstall[@]}" == "" ]] ; then
+        # echo "All packages are already installed.";
+        return;
+    fi;
+
+    # printf "AUR packags not installed:\n%s\n" "${toInstall[@]}";
+    yay --noconfirm -S "${toInstall[@]}" --ask 4;
+}
+
+# ------------------------------------------------------
+# Create symbolic links
+# ------------------------------------------------------
+_installSymLink() {
+    name="$1"
+    symlink="$2";
+    linksource="$3";
+    linktarget="$4";
+    
+    if [ -L "${symlink}" ]; then
+        rm ${symlink}
+        ln -s ${linksource} ${linktarget} 
+        echo ":: Symlink ${linksource} -> ${linktarget} created."
+    else
+        if [ -d ${symlink} ]; then
+            rm -rf ${symlink}/ 
+            ln -s ${linksource} ${linktarget}
+            echo ":: Symlink for directory ${linksource} -> ${linktarget} created."
+        else
+            if [ -f ${symlink} ]; then
+                rm ${symlink} 
+                ln -s ${linksource} ${linktarget} 
+                echo ":: Symlink to file ${linksource} -> ${linktarget} created."
+            else
+                ln -s ${linksource} ${linktarget} 
+                echo ":: New symlink ${linksource} -> ${linktarget} created."
+            fi
+        fi
+    fi
+}
+
+# ------------------------------------------------------
+# Installation in a KVM Virtual Machine
+# ------------------------------------------------------
+_isKVM() {
+    iskvm=$(sudo dmesg | grep "Hypervisor detected")
+    if [[ "$iskvm" =~ "KVM" ]] ;then
+        echo 0
+    else
+        echo 1
+    fi
+}
+
+# _replaceInFile $startMarket $endMarker $customtext $targetFile
 _replaceInFile() {
 
     # Set function parameters
     start_string=$1
     end_string=$2
     new_string="$3"
-    file_path=$4
+    file_path="$4"
 
     # Counters
     start_line_counter=0
@@ -112,22 +273,3 @@ _replaceLineInFile() {
         sleep 2
     fi
 }
-
-# _writeSettings $settingsFile $customtext
-_writeSettings() {
-    if [ ! -f $1 ] ;then
-        touch $1
-    fi
-    echo "$2" > $1
-}
-
-# _writeConf conf/monitor.conf $sel
-_writeConf() {
-    if [ ! -z $2 ] ;then
-        editsel=$(echo "$installFolder/conf/$3/$2" | sed "s+"\/home\/$USER"+~+")
-        echo "source = $editsel" > $installFolder/conf/$1
-    fi
-}
-
-# Reload Waybar
-# setsid $HOME/dotfiles/waybar/launch.sh 1>/dev/null 2>&1 &
