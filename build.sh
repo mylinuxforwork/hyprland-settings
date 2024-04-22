273e4066b3f0cc70bec3affbd7a965eb7971b48f
Stephan Raabe <mail@stephan-raabe.de>
2024-04-22 21:08:03 +0200

    Updates

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+rm -rf ~/tmp
+mkdir ~/tmp
+cp -r . ~/tmp
+rm -rf ~/tmp/.git
+rm ~/tmp/.gitignore
 cd ..
-ARCH=x86_64 appimagetool ml4w-hyprland-settings
+ARCH=x86_64 appimagetool tmp
 echo ":: AppImage created"
-cp ML4W_Hyprland_Settings-x86_64.AppImage ~/ml4w-hyprland-settings/
+# cp ML4W_Hyprland_Settings-x86_64.AppImage ~/ml4w-hyprland-settings/
 cp ML4W_Hyprland_Settings-x86_64.AppImage ~/dotfiles-versions/dotfiles/apps/
 echo ":: AppImage copied to ~/dotfiles-versions/dotfiles/apps/"
