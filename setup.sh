4fe8e2f9e7fb32c49ed2ebb59f79fd78314172be
Stephan Raabe <mail@stephan-raabe.de>
2024-05-06 12:08:51 +0200

    Updates

---
+++ setup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+clear
+
+# Some colors
+GREEN='\033[0;32m'
+NONE='\033[0m'
+
+# Header
+echo -e "${GREEN}"
+cat <<"EOF"
+ ____       _               
+/ ___|  ___| |_ _   _ _ __  
+\___ \ / _ \ __| | | | '_ \ 
+ ___) |  __/ |_| |_| | |_) |
+|____/ \___|\__|\__,_| .__/ 
+                     |_|    
+
+EOF
+echo "for ML4W Hyprland Settings App"
+echo
+echo -e "${NONE}"
+
+echo "This script will download the ML4W Hyprland Settings App and start the installation."
+echo
+while true; do
+    read -p "DO YOU WANT TO START THE INSTALLATION NOW? (Yy/Nn): " yn
+    case $yn in
+        [Yy]* )
+            echo "Installation started."
+            echo
+        break;;
+        [Nn]* ) 
+            echo "Installation canceled."
+            exit;
+        break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# Change into your Downloads directory
+cd ~/Downloads
+
+# Remove existing folder
+if [ -d ml4w-hyprland-settings ] ;then
+    rm -rf ml4w-hyprland-settings
+fi
+
+# Clone the packages
+git clone --depth 1 https://gitlab.com/stephan-raabe/ml4w-hyprland-settings.git
+
+# Change into the folder
+cd ml4w-hyprland-settings
+
+echo ":: Starting the installation now..."
+sleep 3
+
+# Start the script
+./install.sh
