989bf7ce9e0ad5c8d78177f28c9b6d0566bca7df
Stephan Raabe <mail@stephan-raabe.de>
2025-07-01 10:36:49 +0200

    Update

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@
 # ----------------------------------------------------------
 
 app="com.ml4w.hyprlandsettings"
+repo="ml4w-repo"
 public_key="ml4w-apps-public-key.asc"
 public_key_url="https://mylinuxforwork.github.io/ml4w-flatpak-repo/$public_key"
 
@@ -26,13 +27,45 @@ _commandExists() {
 # ----------------------------------------------------------
 
 _is_flatpak_repo_installed() {
-  local repo_name="$1"
-  flatpak_output=$(flatpak remotes)
-  if [[ $flatpak_output == *"$repo_name"* ]]; then
+	local repo_name="$1"
+	flatpak_output=$(flatpak remotes)
+	if [[ $flatpak_output == *"$repo_name"* ]]; then
+		echo ":: Repo '$repo_name' is already added."
+		return 0
+	else
+		echo ":: Repo '$repo_name' is NOT added."
+		return 1
+	fi
+}
+
+# ----------------------------------------------------------
+# Check if flatpak is already installed in repo
+# ----------------------------------------------------------
+
+_is_flatpak_installed() {
+  if flatpak list --columns=application,origin | grep -qE "^$app\s+$repo$"; then
+    echo ":: Flatpak '$app' from repository '$repo' is installed."
     return 0
   else
+    echo ":: Flatpak '$app' from repository '$repo' is NOT installed."
     return 1
   fi
+}
+
+# ----------------------------------------------------------
+# Check if flatpak is user installed
+# ----------------------------------------------------------
+
+_is_flatpak_installed_user() {
+	flatpak_output=$(flatpak list --user --columns=application,origin)
+	if [[ $flatpak_output == *"$app"* ]]; then
+		echo ":: Flatpak '$app' is installed for the current user but should be installed system-wide."
+		echo ":: It will now be uninstalled first."
+		return 0
+	else
+		echo ":: Flatpak '$app' is NOT installed for the current user."
+		return 1
+	fi
 }
 
 # ----------------------------------------------------------
@@ -57,10 +90,7 @@ fi
 # Adding flathub
 # ----------------------------------------------------------
 
-if _is_flatpak_repo_installed "flathub"; then
-	echo ":: flathub is already added."
-else
-	echo ":: Adding flathub"
+if ! _is_flatpak_repo_installed "flathub"; then
 	sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 fi
 
@@ -68,9 +98,7 @@ fi
 # Adding ml4w-repo
 # ----------------------------------------------------------
 
-if _is_flatpak_repo_installed "ml4w-repo"; then
-	echo ":: ml4w-repo is already added."
-else
+if ! _is_flatpak_repo_installed "ml4w-repo"; then
 	echo ":: Downloading Public Key"
 	if [ ! -d "$HOME/.cache" ]; then
 		mkdir -p "$HOME/.cache"
@@ -83,23 +111,36 @@ else
 		echo "ERROR: Download of Public Key failed."
 		exit
 	fi	
-	echo ":: Adding ml4w-repo"
 	sudo flatpak remote-add --if-not-exists ml4w-repo https://mylinuxforwork.github.io/ml4w-flatpak-repo/ml4w-apps.flatpakrepo --gpg-import=$HOME/.cache/$public_key
 fi
-
-# ----------------------------------------------------------
-# Install app
-# ----------------------------------------------------------
-
-echo ":: Installing $app"
-sudo flatpak install -y --reinstall ml4w-repo $app
 
 # ----------------------------------------------------------
 # Cleanup
 # ----------------------------------------------------------
 
 if [ -f "$HOME/.cache/$public_key" ]; then
+	echo ":: Removing public key of $repo"
 	rm "$HOME/.cache/$public_key"
+fi
+
+# ----------------------------------------------------------
+# Uninstall app from current user
+# ----------------------------------------------------------
+
+if _is_flatpak_installed_user "$app"; then
+	flatpak uninstall -y --user $app
+fi
+
+# ----------------------------------------------------------
+# Install app
+# ----------------------------------------------------------
+
+if _is_flatpak_installed $app ml4w-repo; then
+	echo ":: Checking updates for '$app'"
+	flatpak -y update $app
+else
+	echo ":: Installing $app"
+	sudo flatpak install -y --reinstall ml4w-repo $app
 fi
 
 # ----------------------------------------------------------
@@ -107,4 +148,4 @@ fi
 # ----------------------------------------------------------
 
 echo
-echo ":: Setup complete. Run the app with flatpak run $app"
+echo ":: Setup complete. Run the app with 'flatpak run $app'"
